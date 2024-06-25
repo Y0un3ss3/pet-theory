0fcf35dc82ec906237ce76da504e70a476a8ba64
PI <26528746+Y0un3ss3@users.noreply.github.com>
2024-06-25 15:44:14 +0200

    Update index.js

--- lab05/solution/email-service/index.js
+++ lab05/solution/email-service/index.js
@@ -1,20 +1,28 @@
-const util = require('./common/util.js');
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
 
 app.post('/', async (req, res) => {
-  const labReport = util.decodeBase64Json(req.body.message.data);
   try {
-    const status = await util.attemptFlakeyOperation();
+    const labReport = req.body;
+    await publishPubSubMessage(labReport);
     res.status(204).send();
   }
   catch (ex) {
-    res.status(500).send();
+    console.log(ex);
+    res.status(500).send(ex);
   }
 })
+
+async function publishPubSubMessage(labReport) {
+  const buffer = Buffer.from(JSON.stringify(labReport));
+  await pubsub.topic('new-lab-report').publish(buffer);
+}
